6f68f5e3ac1b9167bb87bbc807a17a4266af0b7c
macpuli <53869350+macpuli@users.noreply.github.com>
2019-08-16 16:49:25 -0500

    Cambio qDebug
    Faltaba librería

--- SVEA/mainwindow.cpp
+++ SVEA/mainwindow.cpp
@@ -1,12 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include "QDebug"
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QDebug('Hola Martin');
+    qDebug() << "Daniel";
 }
 
 MainWindow::~MainWindow()
