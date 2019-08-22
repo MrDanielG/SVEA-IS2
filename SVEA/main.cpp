9cec49a1097dcd56ca56794d9585fa268a8b6ecc
macpuli <53869350+macpuli@users.noreply.github.com>
2019-08-21 20:00:32 -0500

    08:00 21 ago

--- SVEA/main.cpp
+++ SVEA/main.cpp
@@ -11,6 +11,8 @@ int main(int argc, char *argv[])
 
     a.setStyle(QStyleFactory::create("Fusion"));
     QPalette p = a.palette();
+
+            p.setColor(QPalette::Background, QColor(255,255,255));
     p.setColor(QPalette::Window, QColor(255,255,255));
 //    p.setColor(QPalette::Button, QColor(53,53,53));
     p.setColor(QPalette::Highlight, QColor(135,190,171));
